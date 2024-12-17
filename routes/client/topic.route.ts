651fbf3d2c83273eecc1829e7878dca077d573a9
chison2707 <chiphansonzz17@gmail.com>
2024-12-17 21:55:22 +0700

    tạo controller

--- routes/client/topic.route.ts
+++ routes/client/topic.route.ts
@@ -1,12 +1,6 @@
-import { Request, Response, Router } from 'express';
+import { Router } from 'express';
 const router: Router = Router();
-import Topic from '../../models/topic.model';
-router.get('/', async (req: Request, res: Response) => {
-    const topics = await Topic.find({
-        deleted: false
-    });
-    console.log(topics)
-    res.render("client/page/topics/index");
-});
+import * as controller from "../../controllers/client/topic.controller";
+router.get('/', controller.topics);
 
 export const topicRoutes: Router = router;
