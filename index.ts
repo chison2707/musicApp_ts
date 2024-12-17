77f433882931e7d6f124e33ae170ad394835f29d
chison2707 <chiphansonzz17@gmail.com>
2024-12-17 16:32:25 +0700

    tạo router cơ bản

--- index.ts
+++ index.ts
@@ -1,1 +1,11 @@
-console.log("ok")
+import express, { Express, Request, Response } from 'express';
+const app: Express = express();
+const port: number = 3000;
+
+app.get('/topics', (req: Request, res: Response) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
