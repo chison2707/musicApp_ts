674f0ed329da72582bd2e1310c91fcf5551a1a9c
chison2707 <chiphansonzz17@gmail.com>
2024-12-17 16:39:43 +0700

    cài đặt pug

--- index.ts
+++ index.ts
@@ -2,10 +2,13 @@ import express, { Express, Request, Response } from 'express';
 const app: Express = express();
 const port: number = 3000;
 
+app.set("views", "./views");
+app.set('view engine', 'pug');
+
 app.get('/topics', (req: Request, res: Response) => {
-    res.send('Hello World!')
-})
+    res.render("client/page/topics/index");
+});
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
-})
+});
