47b6927fec076ae0a1e96d47df2856684902c282
chison2707 <chiphansonzz17@gmail.com>
2024-12-21 16:21:25 +0700

    tính năng nghe thử audio

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -3,7 +3,7 @@ const uploadImage = document.querySelector("[upload-image]");
 if (uploadImage) {
     const uploadImageInput = document.querySelector("[upload-image-input]");
     const uploadImagePreview = document.querySelector("[upload-image-preview]");
-    const removeImage = document.querySelector("[removeImage]");
+    // const removeImage = document.querySelector("[removeImage]");
 
     uploadImageInput.addEventListener("change", (e) => {
         const file = e.target.files[0];
@@ -12,10 +12,29 @@ if (uploadImage) {
             removeImage.style.display = 'block';
         }
     });
-    removeImage.addEventListener("click", () => {
-        uploadImageInput.value = '';
-        uploadImagePreview.src = '';
-        removeImage.style.display = 'none';
-    })
+    // removeImage.addEventListener("click", () => {
+    //     uploadImageInput.value = '';
+    //     uploadImagePreview.src = '';
+    //     removeImage.style.display = 'none';
+    // })
 }
 // end upload image
+
+// upload audio
+const uploadAudio = document.querySelector("[upload-audio]");
+if (uploadAudio) {
+    const uploadAudioInput = uploadAudio.querySelector("[upload-audio-input]");
+    const uploadAudioPlay = uploadAudio.querySelector("[upload-audio-play]");
+    const source = uploadAudio.querySelector("source");
+
+    uploadAudioInput.addEventListener("change", (e) => {
+        const file = e.target.files[0];
+        if (file) {
+            const audio = URL.createObjectURL(file);
+
+            source.src = audio;
+            uploadAudioPlay.load();
+        }
+    });
+}
+// end upload audio
